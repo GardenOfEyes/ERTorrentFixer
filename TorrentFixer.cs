6da3f82362e8afa2031a11f1b1a6f99a92b9a379
GardenOfEyes <86174652+GardenOfEyes@users.noreply.github.com>
2023-04-23 10:55:54 +0300

    Update TorrentFixer.cs

--- TorrentFixer.cs
+++ TorrentFixer.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Loads the SoulsFormat library
 using SoulsFormats;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Numerics;
-using Assimp.Unmanaged;
 
 // Get the file path from the command line arguments
 string bndFile = args[0];
