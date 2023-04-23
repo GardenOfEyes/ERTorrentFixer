1e059d1e06578e57f80a094f2041427a05886c2e
GardenOfEyes <86174652+GardenOfEyes@users.noreply.github.com>
2023-04-23 10:47:18 +0300

    Create TorrentFixer.cs

---
+++ TorrentFixer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoulsFormats;
+using SoulsAssetPipeline.Animation;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Numerics;
+using Assimp.Unmanaged;
+
+
+
+    string bndFile = args[0];
+
+    //string bndFile = "D:\\SteamLibrary\\steamapps\\common\\ELDEN RING\\Game\\TheGardenOfEyes\\chr\\New folder\\c8000.chrbnd.dcx";
+
+    //string bndFile = File.ReadAllText(filePath);
+
+
+    BND4 bnd = BND4.Read(bndFile);
+    IEnumerable<BinderFile> flverFiles = bnd.Files.Where(x => x.Name.Contains(".flver"));
+    foreach (var flverFile in flverFiles.Where(x => x.Bytes.Length > 0))
+    {
+        FLVER2 flver = FLVER2.Read(flverFile.Bytes);
+        flverFile.Bytes = flver.Write();
+        bnd.Write(bndFile, DCX.Type.DCX_KRAK);
+    }
+bnd.Write(bndFile, DCX.Type.DCX_KRAK);
